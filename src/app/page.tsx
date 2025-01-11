95d6d5f05ccc6a94b8768c9871ad41ee257739f4
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2025-01-11 21:05:49 +0530

    changed the background color for homepage https://hypercolor.dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-green-200 via-green-300 to-blue-500">
       {/* this centre anything in dev */}
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
